e1e75e73fa4839ad6ffd434fab5263902fd91c98
gabsommer <gabr.sommer@gmail.com>
2025-07-21 10:01:38 +0200

    add date and add automatic data skip according to parameters

--- anim/anim.py
+++ anim/anim.py
@@ -5,13 +5,12 @@ import cartopy.feature as cfeature
 import pandas as pd
 from matplotlib.patches import Polygon
 from matplotlib.animation import FuncAnimation
+from datetime import datetime, timedelta
 
 from utils import clean_hull, lon_lat_split, clean, get_eclipses, clean_hull2
 
 
-#import values from config file
 config = {}
-
 with open("../main.conf", "r") as configfile:
     for line in configfile:
         line = line.strip()
@@ -19,16 +18,20 @@ with open("../main.conf", "r") as configfile:
             continue
         key, value = line.split("=", 1)
         config[key.strip()] = value.strip()
-
 umbra_res = config['umbra_res']
 penumbra_res = config['penumbra_res']
-animlength = config['animlength']
+init_year = config['init_year']
+init_month = config['init_month']
+init_day = config['init_day']
+init_hour = config['init_hour']
+init_min = config['init_min']
+init_second = config['init_second']
+init_dt = datetime(int(init_year), int(init_month), int(init_day), int(init_hour), int(init_min), int(init_second))
 
-print(f"[debug] Penumbra resolution: {penumbra_res}")
 eclipses = get_eclipses("../data/split")
 eclipse = 0
-skip = 5
-test_frame = 400
+
+
 
 
 #print(f"[info] \033[34m/data/lonlat.dat\033[0m is being processed...")
@@ -42,24 +45,30 @@ data_umbra = np.loadtxt("../data/split/" + eclipses[eclipse] + "lonlat_umbra.dat
 data_umbra = data_umbra.reshape(int(int(data_umbra.shape[0])/int(umbra_res)),int(umbra_res),3)
 data_penumbra = np.loadtxt("../data/split/" + eclipses[eclipse] + "lonlat_penumbra.dat", delimiter=",")
 data_penumbra = data_penumbra.reshape(int(int(data_penumbra.shape[0])/int(penumbra_res)),int(penumbra_res),3)
-#TODO Clean data such that outliers are properly removed. There are numerical artefacts in the plot (umbra_plot.png).
+
+#video parameters
+total_frames = data_umbra.shape[0]
+if total_frames != data_penumbra.shape[0]:
+    print(f"\033[38;5;208m[warning]\033[0m Total frames in umbra ({total_frames}) and penumbra ({data_penumbra.shape[0]}) do not match.")
+animlength = int(config['animlength'])
+skip = total_frames//(int(config["animlength"])*int(config['animfps']))
 
 
 data_umbra = data_umbra[::skip,:,:]
-data_umbra_clean = clean(data_umbra[test_frame,:,:2])
-data_umbra_clean_hull = clean_hull2(data_umbra[test_frame,:,:2])
+data_umbra_clean = clean(data_umbra[0,:,:2])
+data_umbra_clean_hull = clean_hull2(data_umbra_clean)
 lon = data_umbra_clean[:, 0]
 lat = data_umbra_clean[:, 1]
 
 
 
 data_penumbra = data_penumbra[::skip,:,:]
-data_penumbra_clean = clean(data_penumbra[test_frame,:,:2])
-data_penumbra_clean_hull = clean_hull2(data_penumbra_clean[:,:2])
+data_penumbra_clean = clean(data_penumbra[0,:,:2])
+data_penumbra_clean_hull = clean_hull2(data_penumbra_clean)
 lon_pen = data_penumbra_clean_hull[:,0]
 lat_pen = data_penumbra_clean_hull[:,1]
-print(f"[debug] Data shape penumbra_clean: {data_penumbra_clean.shape}\n[debug] Data shape umbra_clean: {data_umbra_clean.shape}")
-print(f"[debug] Data shape penumbra_clean_hull: {data_penumbra_clean_hull.shape}\n[debug] Data shape umbra_clean_hull: {data_umbra_clean_hull.shape}")
+#print(f"[debug] Data shape penumbra_clean: {data_penumbra_clean.shape}\n[debug] Data shape umbra_clean: {data_umbra_clean.shape}")
+#print(f"[debug] Data shape penumbra_clean_hull: {data_penumbra_clean_hull.shape}\n[debug] Data shape umbra_clean_hull: {data_umbra_clean_hull.shape}")
 
 #Plot Setup
 fig = plt.figure(figsize=(10, 5))
@@ -83,13 +92,10 @@ polygon = Polygon(np.column_stack((lon, lat)), closed=True, facecolor='black', a
 polygon_penumbra = Polygon(np.column_stack((lon_pen, lat_pen)), closed=True, facecolor='black', alpha=0.2, transform=ccrs.PlateCarree())
 ax.add_patch(polygon)
 ax.add_patch(polygon_penumbra)
-ax.scatter(lon,lat,transform=ccrs.PlateCarree())
-ax.scatter(lon_pen,lat_pen,transform=ccrs.PlateCarree())
-plt.savefig(f"./testframe{test_frame}_plot.png", dpi=600, bbox_inches='tight')
-print(f"[info] File saved as \033[34mtestframe{test_frame}_plot.png\033[0m")
 
 #Animation
 def update(frame):
+    seconds = int(data_umbra[frame,0,2])
     # umbra frames
     new_data_umbra_clean = clean(data_umbra[frame,:,:2])
     new_data_umbra_clean_hull = clean_hull2(new_data_umbra_clean)
@@ -100,6 +106,9 @@ def update(frame):
     new_data_penumbra_clean_hull = clean_hull2(new_data_penumbra_clean)
     new_lon_pen = new_data_penumbra_clean_hull[:, 0]
     new_lat_pen = new_data_penumbra_clean_hull[:, 1]
+
+    dt = init_dt + timedelta(seconds=seconds)
+    ax.set_title(f"Path of Totality: {dt} UTC", color = "white")
 
     polygon_penumbra.set_xy(np.column_stack((new_lon_pen, new_lat_pen)))
     polygon.set_xy(np.column_stack((new_lon, new_lat)))
